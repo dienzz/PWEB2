41332d9d48918c8c6c5be30c20bd8947dabb7ae6
dienzz <20230910002@uniku.ac.id>
2025-07-01 08:30:13 +0700

    Initial commit

--- database/seeders/User1Seeder.php
+++ database/seeders/User1Seeder.php
@@ -2,21 +2,18 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User1;
 
 class User1Seeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        DB::table('user1')->insert([
+        User1::create([
             'name' => 'Admin GYM',
             'email' => 'admin@gmail.com',
-            'password' => '@dmin_123', 
+            'password' => Hash::make('@dmin_123'),
             'created_at' => now(),
             'updated_at' => now(),
         ]);
